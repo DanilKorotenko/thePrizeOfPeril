855b56fee43d63841a430bc822aea490666da492
Danil Korotenko <danil.korotenko@gmail.com>
2014-12-12 17:39:35 +0200

    Added empty locations.

--- main.lua
+++ main.lua
@@ -15,12 +15,12 @@ main = room
 --	end,
 	dsc = "This game is based on Robert Shekley novell."..[[^^
 	Implementation: Danil Korotenko (danil.korotenko@gmail.com).]],
-	obj = { vway("next", "Click {here} to play.", 'hotel') }
+	obj = { vway("next", "Click {here} to play.", 'hotel_room') }
 };
 
-hotel = room 
+hotel_room = room 
 {
-	nam = 'Hotel',
+	nam = 'Hotel Room',
 	dsc = [[RAEDER lifted his head cautiously above the windowsill. He saw the fire-escape, and below it a narrow alley. There was a weather-beaten baby carriage in the alley and three garbage cans. As he watched, a black-sleeved arm moved from behind the furthest can, with something shiny in its fist. Raeder ducked down. A bullet smashed through the window above his head and punctured the ceiling, showering him with plaster.
 Now he knew about the alley. It was guarded, just like the door.
 He lay at full length on the cracked linoleum, staring at the bullet hole in the ceiling, listening to the sounds outside the door. He was a tall man with bloodshot eyes and a two-day stubble. Grime and fatigue had etched lines into his face. Fear had touched his features, tightening a muscle here and twitching a nerve there. The results were startling. His face had character now, for it was reshaped by the expectation of death.
@@ -50,4 +50,106 @@ Raeder waited, and heard the hinges tearing out of rotten wood.
 Raeder pushed the television set into his pocket. He located the outlines of the window and kicked. Glass shattered, and daylight poured startlingly in. He cleared the jagged sill and quickly peered down.
 Below was a long drop to a concrete courtyard.
 The hinges tore free. He heard the door opening.]],
+}
+
+hotel_bathroom = room 
+{
+	nam = 'Hotel Bathroom',
+	dsc = [[]]
+}
+
+courtyard = room 
+{
+	nam = 'Courtyard',
+	dsc = [[]]
+}
+
+street = room 
+{
+	nam = '63rd Street',
+	dsc = [[]]
+}
+
+mrs_velma_o_dell_house = room 
+{
+	nam = "Mrs. Velma O'Dell house",
+	dsc = [[]]
+}
+
+subway = room 
+{
+	nam = "Subway",
+	dsc = [[]]
+}
+
+subway_flashback = room 
+{
+	nam = "Subway Flashback",
+	dsc = [[]]
+}
+
+manhasset_station = room 
+{
+	nam = "Manhasset station",
+	dsc = [[]]
+}
+
+manhasset_station = room 
+{
+	nam = "Manhasset station",
+	dsc = [[]]
+}
+
+taxi = room 
+{
+	nam = "Taxi",
+	dsc = [[]]
+}
+
+country_road = room 
+{
+	nam = "Country Road",
+	dsc = [[]]
+}
+
+concrete_highway = room 
+{
+	nam = "Concrete Highway",
+	dsc = [[]]
+}
+
+janice_morrow_car = room 
+{
+	nam = "Janice Morrow car",
+	dsc = [[]]
+}
+
+inhabitant_zone = room 
+{
+	nam = "Inhabitant Zone",
+	dsc = [[]]
+}
+
+inhabitant_zone_flashback = room 
+{
+	nam = "Inhabitant Zone Flashback",
+	dsc = [[]]
+}
+
+church = room 
+{
+	nam = "Church",
+	dsc = [[]]
+}
+
+graveyard = room 
+{
+	nam = "Graveyard",
+	dsc = [[]]
+}
+
+open_grave = room 
+{
+	nam = "Open Grave",
+	dsc = [[]]
 }
